70ce52b813ae76d33d42f1f1dd3b91d1eb9949fe
VitaliyOne <vitaliy.one1@yandex.ru>
2024-02-21 22:11:25 +0300

    adding dark theme, code  review

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,8 @@ const portfolioButton = document.getElementById("portfolio");
 const footer = document.querySelector('.footer');
 const footerButton = document.getElementById("footer");
 const ageBlock = document.getElementById('age')
-
+const mainAboutMeIcon = document.querySelector(".mainAboutMeIcon");
+let mainImageElement = document.createElement("img");
 
 function handleScrollButtonClick(block, positionScroll = "center") {
     if (block){
@@ -36,3 +37,38 @@ const getAge = () =>{
     if (age && ageBlock) {ageBlock.innerHTML = age}
 }
 window.onload = getAge;
+
+let iconBox = document.querySelector(".mainAboutMeIcon");
+let checkTheme = "";
+
+const getIcon = (theme) =>{
+    if (mainImageElement && mainAboutMeIcon && theme) {
+        theme === "dark"?
+        (mainImageElement.src = "./src/img/darkMode.png")
+        :
+        (mainImageElement.src = "./src/img/lightMode.png")
+        mainAboutMeIcon.appendChild(mainImageElement)
+    }
+}
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  checkTheme = "dark";
+  getIcon("dark")
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  checkTheme = "light";
+  getIcon("light")
+}
+
+iconBox.addEventListener("click", () => {
+    checkTheme === "light" ?
+        (document.documentElement.setAttribute("data-theme", "dark"),
+        checkTheme = "dark",
+        getIcon("dark"))
+    :
+        (document.documentElement.setAttribute("data-theme", "light"),
+        checkTheme = "light",
+        getIcon("light"))
+    }
+);
